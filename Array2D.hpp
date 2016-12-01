6ccb971888626cf68c4c84efb6f49f54d457d441
matthew reigada <mreigada@matthews-MacBook-Pro-7.local>
2016-12-01 14:16:21 -0600

    copy constructor, out of range assert error

--- Array2D.hpp
+++ Array2D.hpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 template<class T> class Array2D {
     public:
         T* array;
@@ -9,15 +10,38 @@ template<class T> class Array2D {
             w(_w),
             array(new T[_h * _w]) {
         }
+
+        Array2D(const Array2D<T>& other) : h(other.h), w(other.w), array(NULL) {
+            array = new T[h * w];
+            for (int i = 0; i < h * w; i++) {
+                array[i] = other.array[i];
+            }
+        }
         
         ~Array2D() {
             delete[] array;
             array = NULL;
         }
     
-        //writes pointed value, therefore will lock
+        //returns address of first item in row
         T* operator[](const unsigned int k) {
+            assert(k < h);
             return &array[k * w];
+        }
+
+        //copies each data item in array into new array
+        Array2D<T>& operator=(const Array2D<T>& other) {
+            if (NULL != array) {
+                delete[] array;
+                array = NULL;
+            }
+            h = other.h;
+            w = other.w;
+            array = new T[h * w];
+            for (int i = 0; i < h * w; i++) {
+                array[i] = other.array[i];
+            }
+            return *this;
         }
 };
 
